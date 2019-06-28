e7f5f190393b4a7f810af1efbf54a92c843b845c
Héctor García <hgarcia@firstmajestic.com>
2019-06-28 15:59:36 -0500

    Change Title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="This my First Vue.js App"/>
   </div>
 </template>
 
